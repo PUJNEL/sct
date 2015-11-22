a4c4b1211e84b446e8eb18482373ce6a3472d7aa
adgranados <adgranados@gmail.com>
2015-11-22 11:31:41 -0500

    Base Completa

---
+++ vendor/cakephp-plugins.php
@@ -0,0 +1,10 @@
+<?php
+$baseDir = dirname(dirname(__FILE__));
+return [
+    'plugins' => [
+        'Migrations' => 'C:\wamp\www\sct/vendor/cakephp/migrations/',
+        'DebugKit' => 'C:\wamp\www\sct/vendor/cakephp/debug_kit/',
+        'Bake' => $baseDir . '/vendor/cakephp/bake/',
+
+    ]
+];
